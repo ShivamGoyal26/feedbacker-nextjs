830cfc9f395db40beebf9f0e157db50aaaa9023f
shivamgoyal26 <officialshivamgoyal@gmail.com>
2024-08-05 18:53:09 +0530

    Get user message status api added

--- src/app/api/accept-messages/route.ts
+++ src/app/api/accept-messages/route.ts
@@ -60,3 +60,54 @@ export async function POST(request: Request) {
     );
   }
 }
+
+export async function GET(request: Request) {
+  await dbConnect();
+
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user as User;
+
+  if (!session || !session.user) {
+    return Response.json(
+      {
+        success: false,
+        message: "Not authenticated",
+      },
+      { status: 401 }
+    );
+  }
+
+  const userId = user._id;
+
+  try {
+    const foundUser = await UserModel.findById(userId);
+
+    if (!foundUser) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found",
+        },
+        { status: 404 }
+      );
+    } else {
+      return Response.json(
+        {
+          success: true,
+          message: "Message status found",
+          isAcceptingMessage: foundUser.isAcceptingMessage,
+        },
+        { status: 200 }
+      );
+    }
+  } catch (error) {
+    console.log("failed to get the accepting message status", error);
+    return Response.json(
+      {
+        success: false,
+        message: "failed to get the accepting message status",
+      },
+      { status: 500 }
+    );
+  }
+}
